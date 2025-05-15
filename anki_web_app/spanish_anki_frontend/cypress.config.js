cf8c17a9aa0fe60ee2fa9554ab7b2584756c46df
beduffy <benfduffy@gmail.com>
2025-05-16 00:35:39 +0200

    Increase default command timeout in Cypress configuration for CI

--- anki_web_app/spanish_anki_frontend/cypress.config.js
+++ anki_web_app/spanish_anki_frontend/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000, // Increased timeout for CI
   e2e: {
     baseUrl: 'http://localhost:8080',
     specPattern: 'cypress/e2e/*.cy.js',
